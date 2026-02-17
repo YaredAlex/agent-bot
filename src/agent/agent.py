a8a55e091a1fa9b3f730e4e53417f06630461308
YaredAlex <yareda25@gmail.com>
2026-02-17 15:56:35 +0300

    fix: changed from pydantic.v1 to pydantic

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -15,13 +15,11 @@ from langgraph.graph import MessagesState,START,END,StateGraph
 from langgraph.prebuilt import ToolNode,tools_condition
 from langchain_ollama.chat_models import ChatOllama
 from typing import TypedDict, Literal,Optional
-from pydantic.v1 import BaseModel,validator,Field
+from pydantic import BaseModel,Field
 from trustcall import create_extractor
 #Setting environment
-os.environ["LANGSMITH_API_KEY"]= "KEY"
-os.environ["LANGSMITH_TRACING"] = "true"
-os.environ["LANGSMITH_PROJECT"] = ""
-
+os.environ["LANGCHAIN_TRACING"] = "true"
+os.environ["LANGSMITH_PROJECT"]="My First App"
 #CheckPointer
 conn_string = "postgresql://postgres:root@localhost:5432/agent_bot"
 # pool = ConnectionPool(conn_string, kwargs={"autocommit": True})
@@ -45,7 +43,7 @@ Here is the current User Profile (may be empty if no information has been collec
 
 Here is the User shopping histry (may be empty if no purchase have been added yet):
 <history>
-{todo}
+{history}
 </history>
 
 Here are the current user-specified preferences for updating the preference list (may be empty if no preferences have been specified yet):
@@ -58,25 +56,21 @@ Here are your instructions for reasoning about the user's messages:
 1. Reason carefully about the user's messages as presented below. 
 
 2. Decide whether any of the your long-term memory should be updated:
-- If personal information was provided about the user, update the user's profile by calling UpdateMemory tool with type `user`
-- If tasks are mentioned, update the ToDo list by calling UpdateMemory tool with type `todo`
-- If the user has specified preferences for how to update the ToDo list, update the instructions by calling UpdateMemory tool with type `instructions`
+- If personal information was provided about the user, update the user's profile by calling update_profile tool
+- If needed update user history when updating profile to personalize conversation
 
 3. Tell the user that you have updated your memory, if appropriate:
 - Do not tell the user you have updated the user's profile
-- Tell the user them when you update the todo list
 - Do not tell the user that you have updated instructions
 
-4. Err on the side of updating the todo list. No need to ask for explicit permission.
+4. Err on the side of updating profile or history. No need to ask for explicit permission.
 
 5. Respond naturally to user user after a tool call was made to save memories, or if no tool call was made."""
 
 # Trustcall instruction
 TRUSTCALL_INSTRUCTION = """Reflect on following interaction. 
 
-Use the provided tools to retain any necessary memories about the user. 
-
-Use parallel tool calling to handle updates and insertions simultaneously.
+Use the provided tools to retain any necessary memories about the user.
 
 System Time: {time}"""
 
@@ -97,6 +91,9 @@ class AssistantAgent:
         else:
             self.memory = InMemorySaver()
             self.store = InMemoryStore()
+        #  NOTE: you need to call .setup() the first time you're using your checkpointer
+        self.memory.setup()
+        self.store.setup()
         # binding tool
         self.llm = llm
         self.tools = tools + AgentMemoryTools(memory=self.memory,store=self.store,llm=self.llm).get_tools()
@@ -124,12 +121,12 @@ class AssistantAgent:
                 user_profile = memories[0].value
             else:
                 user_profile = None
-
+            print("user profile ",user_profile)
             # retrive history
             namespace = ("history", user_id)
             memories = store.search(namespace)
             history = "\n".join(f"{mem.value}" for mem in memories)
-
+            print("user history ",history)
             # Retrieve custom instructions
             namespace = ("instructions", user_id)
             memories = store.search(namespace)
@@ -144,14 +141,131 @@ class AssistantAgent:
 
             return {"messages": [response]}
         
+        profile_extractor = create_extractor(
+            self.llm,
+            tools=[Profile],
+            tool_choice="Profile",
+        )
         
+        def update_profile(state: MessagesState, config: RunnableConfig, store: BaseStore):
+            """Reflect on the chat history and update the memory collection."""
+            
+            # Get the user ID from the config
+            user_id = config["configurable"]["user_id"]
+
+            # Define the namespace for the memories
+            namespace = ("profile", user_id)
+            # Retrieve the most recent memories for context
+            existing_items = store.search(namespace)
+            print("Exisiting_items ", existing_items)
+            # Format the existing memories for the Trustcall extractor
+            tool_name = "Profile"
+            existing_memories = ([(existing_item.key, tool_name, existing_item.value)
+                                for existing_item in existing_items]
+                                if existing_items
+                                else None
+                                )
+            print("Exisiting memories ",existing_memories)
+            # Merge the chat history and the instruction
+            TRUSTCALL_INSTRUCTION_FORMATTED=TRUSTCALL_INSTRUCTION.format(time=datetime.now().isoformat())
+            updated_messages=list(merge_message_runs(messages=[SystemMessage(content=TRUSTCALL_INSTRUCTION_FORMATTED)] + state["messages"][:-1]))
+
+            # Invoke the extractor
+            result = profile_extractor.invoke({"messages": updated_messages, 
+                                                "existing": existing_memories})
+            print("result of profile extractor ",result)
+            # Save the memories from Trustcall to the store
+            for r, rmeta in zip(result["responses"], result["response_metadata"]):
+                store.put(namespace,
+                        rmeta.get("json_doc_id", str(uuid.uuid4())),
+                        r.model_dump(mode="json"),
+                    )
+            tool_calls = state['messages'][-1].tool_calls
+            return {"messages": [{"role": "tool", "content": "updated profile", "tool_call_id":tool_calls[0]['id']}]}
+        
+        
+        def update_history(state: MessagesState, config: RunnableConfig, store: BaseStore):
+            """Summarize recent conversation and persist rolling history summary."""
+
+            user_id = config["configurable"]["user_id"]
+            namespace = ("history", user_id)
+
+            #  Get existing history summary
+            existing_items = store.search(namespace)
+            existing_summary = existing_items[0].value if existing_items else ""
+
+            # Prepare conversation text (exclude system messages)
+            conversation_text = "\n".join(
+                f"{m.type.upper()}: {m.content}"
+                for m in state["messages"]
+                if hasattr(m, "content")
+            )
+
+            # Create summarization prompt
+            summary_prompt = f"""
+                        You are a conversation memory summarizer.
+                        Existing summary:
+                        {existing_summary}
+                        New conversation:
+                        {conversation_text}
+                        Update the summary to include important long-term context about the user,
+                        their preferences, goals, interests, and important discussion points.
+                        Keep it concise but informative.
+                        """
+            print("summary prompt ",summary_prompt)
+            summary_response = self.llm.invoke(summary_prompt)
+
+            updated_summary = summary_response.content
+
+            # Save updated summary (overwrite old one)
+            store.put(
+                namespace,
+                "rolling_summary",  # fixed key to overwrite
+                updated_summary
+            )
+
+            return "history updated"
+
+        
+        #Node for routing messages
+        def route_message(state: MessagesState,) -> Literal[END,"tools", "update_profile", "update_history" ]:
+
+            last_message = state["messages"][-1]
+
+            # No tool calls 
+            if not getattr(last_message, "tool_calls", None):
+                return END
+
+            tool_call = last_message.tool_calls[0]
+            tool_name = tool_call["name"]
+
+            # Manual memory nodes
+            if tool_name == "update_profile":
+                return "update_profile"
+
+            elif tool_name == "update_history":
+                return "update_history"
+
+            # elif tool_name == "update_instructions":
+            #     return "update_instructions"
+
+            # Everything else (MCP tools)
+            else:
+                return "tools"
+            
+
         #build the graph here
         self.builder = StateGraph(MessagesState)
         self.builder.add_node("assistant",assistant)
         self.builder.add_node("tools",ToolNode(self.tools))
+        self.builder.add_node("update_profile",update_profile)
+        self.builder.add_node("update_history",update_history)
         #adding edge
         self.builder.add_edge(START,"assistant")
-        self.builder.add_conditional_edges("assitant",tools_condition)
+        self.builder.add_conditional_edges("assistant",route_message)
+        self.builder.add_edge("update_profile","assistant")
+        self.builder.add_edge("update_history","assistant")
+        self.builder.add_edge("tools","assistant")
         self.graph = self.builder.compile(checkpointer=self.memory,store=self.store)
         return self.graph
     
@@ -186,49 +300,19 @@ class AgentMemoryTools:
 
     def get_tools(self):
         # Create the Trustcall extractor for updating the user profile 
-        profile_extractor = create_extractor(
-            self.llm,
-            tools=[Profile],
-            tool_choice="Profile",
-        )
-        def update_profile(state: MessagesState, config: RunnableConfig, store: BaseStore):
-            """Reflect on the chat history and update the memory collection."""
-            
-            # Get the user ID from the config
-            user_id = config["configurable"]["user_id"]
-
-            # Define the namespace for the memories
-            namespace = ("profile", user_id)
-
-            # Retrieve the most recent memories for context
-            existing_items = store.search(namespace)
-
-            # Format the existing memories for the Trustcall extractor
-            tool_name = "Profile"
-            existing_memories = ([(existing_item.key, tool_name, existing_item.value)
-                                for existing_item in existing_items]
-                                if existing_items
-                                else None
-                                )
-
-            # Merge the chat history and the instruction
-            TRUSTCALL_INSTRUCTION_FORMATTED=TRUSTCALL_INSTRUCTION.format(time=datetime.now().isoformat())
-            updated_messages=list(merge_message_runs(messages=[SystemMessage(content=TRUSTCALL_INSTRUCTION_FORMATTED)] + state["messages"][:-1]))
-
-            # Invoke the extractor
-            result = profile_extractor.invoke({"messages": updated_messages, 
-                                                "existing": existing_memories})
-
-            # Save the memories from Trustcall to the store
-            for r, rmeta in zip(result["responses"], result["response_metadata"]):
-                store.put(namespace,
-                        rmeta.get("json_doc_id", str(uuid.uuid4())),
-                        r.model_dump(mode="json"),
-                    )
-            tool_calls = state['messages'][-1].tool_calls
-            return {"messages": [{"role": "tool", "content": "updated profile", "tool_call_id":tool_calls[0]['id']}]}
+        def update_profile():
+            """
+            Docstring for update_profile
+            to update user profile based on preference
+            """
+            return "update_profile"
+        def update_history():
+            """
+            Docstring for update_history
+            update conversation history with summary
+            """
         
-        return [update_profile]
+        return [update_history,update_profile]
 
 
 
@@ -244,5 +328,5 @@ agent = AssistantAgent(
 
 config = {"configurable": {"thread_id": "user_id_1", "user_id": "demo_user"}}
 
-for chunk in agent.stream({"messages": "Hello there, did you know about GPT"}, config, stream_mode="values"):
+for chunk in agent.stream({"messages": "could you summarize our conversation?"}, config, stream_mode="values"):
     chunk["messages"][-1].pretty_print()
